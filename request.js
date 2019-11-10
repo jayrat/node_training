11689e3a0008afdbc680137426c3a5b320501efc
Jeremy Hawkins <jayrat28@gmail.com>
2019-11-10 01:12:06 -0600

    eslint setup

--- request.js
+++ request.js
@@ -1,16 +1,16 @@
-const https = require("https");
+const https = require('https')
 
 const options = {
-	hostname: "en.wikipedia.org",
-	port: 443,
-	path: "/wiki/Snoopy",
-	method: "GET"
-};
+    hostname: 'en.wikipedia.org',
+    port: 443,
+    path: '/wiki/Snoopy',
+    method: 'GET'
+}
 
 const request = https.request(options, res => {
-	res.setEncoding("UTF-8");
+    res.setEncoding('UTF-8')
 
-	res.on("data", console.log);
-});
+    res.on('data', console.log)
+})
 
-request.end();
+request.end()
